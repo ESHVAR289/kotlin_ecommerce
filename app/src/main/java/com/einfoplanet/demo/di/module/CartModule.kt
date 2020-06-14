d41323e8e02c0c70af0f765a5457ff52985dad53
ESHVAR MALI <eshvar89@hotmail.com>
2020-06-14 11:09:49 +0530

    - created cart module which gives cart list repository implementation

---
+++ app/src/main/java/com/einfoplanet/demo/di/module/CartModule.kt
@@ -0,0 +1,14 @@
+package com.einfoplanet.demo.di.module
+
+import com.einfoplanet.demo.repository.CartListRepositoryImpl
+import com.einfoplanet.demo.di.scope.FragmentScope
+import com.einfoplanet.demo.domain.booklist.CartListRepository
+import dagger.Binds
+import dagger.Module
+
+@Module
+interface CartModule {
+    @Binds
+    @FragmentScope
+    fun bindCartProductListRepository(userListRepository: CartListRepositoryImpl): CartListRepository
+}
