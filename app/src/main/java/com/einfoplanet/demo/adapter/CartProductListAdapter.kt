ba7276bf9f5c57af8fa89729531502f12583ac5a
ESHVAR MALI <eshvar89@hotmail.com>
2020-06-14 10:56:14 +0530

    - created adapter which is used to show cart product listing

---
+++ app/src/main/java/com/einfoplanet/demo/adapter/CartProductListAdapter.kt
@@ -0,0 +1,47 @@
+package com.einfoplanet.demo.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.einfoplanet.demo.R
+import com.einfoplanet.demo.databinding.CartItemLayoutBinding
+import com.einfoplanet.demo.db.CartProduct
+
+/**
+ * Adapter used to load the photos of selected restaurant.
+ */
+class CartProductListAdapter constructor(
+        private var cartProductsList: List<CartProduct>) : RecyclerView.Adapter<CartProductListAdapter.CustomViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val binding = DataBindingUtil.inflate<CartItemLayoutBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.cart_item_layout, parent, false
+        )
+        return CustomViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return cartProductsList.size
+    }
+
+    fun setList(photosList: List<CartProduct>) {
+        this.cartProductsList = photosList
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        holder.bind(cartProduct = cartProductsList[position])
+    }
+
+    inner class CustomViewHolder(itemView: CartItemLayoutBinding) :
+            RecyclerView.ViewHolder(itemView.root) {
+        private var binding: CartItemLayoutBinding? = null
+
+        fun bind(cartProduct: CartProduct) {
+            if (binding == null)
+                binding = DataBindingUtil.bind(itemView)
+            binding!!.viewModel = cartProduct
+        }
+    }
+}
