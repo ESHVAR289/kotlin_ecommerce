bbf769c289dc19a98a83aeb99cbb8c69daa709d9
eshvar289 <eshvar89@hotmail.com>
2020-06-17 23:14:56 +0530

    - now showing the order placed notification

--- app/src/main/java/com/einfoplanet/demo/ui/cart/CartActivity.kt
+++ app/src/main/java/com/einfoplanet/demo/ui/cart/CartActivity.kt
@@ -1,7 +1,13 @@
 package com.einfoplanet.demo.ui.cart
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import com.einfoplanet.demo.R
 import com.einfoplanet.demo.ShoppingSampleApp
@@ -16,6 +22,7 @@ class CartActivity : AppCompatActivity(), ButtonClickListener {
 
     companion object {
         private const val FRAGMENT_ID = R.id.fragment_container
+        private const val CHANNEL_ID = "12432"
     }
 
     @Inject
@@ -65,11 +72,46 @@ class CartActivity : AppCompatActivity(), ButtonClickListener {
     }
 
     override fun closeButtonClick() {
+        createNotificationChannel()
+        createNotification()
         finish()
     }
 
     override fun buyNowButtonClick() {
 
+    }
+
+    fun createNotification() {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_star)
+                .setContentTitle("Order Confirmed")
+                .setContentText("Arriving on 30/06/2020")
+                .setStyle(NotificationCompat.BigTextStyle()
+                        .bigText("You order is safe with us. Thanks for availing the service." +
+                                " Your order will be arriving on 30/06/2020"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(1243, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
 }
