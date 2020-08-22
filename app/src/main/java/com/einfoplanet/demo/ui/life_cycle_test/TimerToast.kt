e309bffc196eed24024804f16646b9d53d2cf34d
ESHVAR MALI <eshvar89@hotmail.com>
2020-08-22 23:07:23 +0530

    - added the countdown timer and it's overridden methods

--- app/src/main/java/com/einfoplanet/demo/ui/life_cycle_test/TimerToast.kt
+++ app/src/main/java/com/einfoplanet/demo/ui/life_cycle_test/TimerToast.kt
@@ -1,4 +1,17 @@
 package com.einfoplanet.demo.ui.life_cycle_test
 
+import android.os.CountDownTimer
+
 class TimerToast {
+
+    private val countDownTimer = object : CountDownTimer(60000, 3000) {
+        override fun onFinish() {
+
+        }
+
+        override fun onTick(millisUntilFinished: Long) {
+
+        }
+
+    }
 }
