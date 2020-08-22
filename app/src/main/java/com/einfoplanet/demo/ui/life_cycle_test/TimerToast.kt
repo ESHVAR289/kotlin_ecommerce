9ef6771acb4c569340b11cdf85e8f27475d4849f
ESHVAR MALI <eshvar89@hotmail.com>
2020-08-22 23:13:23 +0530

    - added the toast messages in onTick() & onFinish()

--- app/src/main/java/com/einfoplanet/demo/ui/life_cycle_test/TimerToast.kt
+++ app/src/main/java/com/einfoplanet/demo/ui/life_cycle_test/TimerToast.kt
@@ -2,16 +2,17 @@ package com.einfoplanet.demo.ui.life_cycle_test
 
 import android.app.Application
 import android.os.CountDownTimer
+import android.widget.Toast
 
 class TimerToast(application: Application) {
 
     private val countDownTimer = object : CountDownTimer(60000, 3000) {
         override fun onFinish() {
-
+            Toast.makeText(application, "Timer finished", Toast.LENGTH_SHORT).show()
         }
 
         override fun onTick(millisUntilFinished: Long) {
-
+            Toast.makeText(application, "Tick : $millisUntilFinished", Toast.LENGTH_SHORT).show()
         }
 
     }
