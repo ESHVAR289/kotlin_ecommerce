3e316ee0f2e84ecbf06fcb50538ce0efe1be8c1f
ESHVAR MALI <eshvar89@hotmail.com>
2020-08-22 23:19:14 +0530

    - created the instance of timer toast in LifeCycleTestActivity.kt

--- app/src/main/java/com/einfoplanet/demo/ui/life_cycle_test/LifeCycleTestActivity.kt
+++ app/src/main/java/com/einfoplanet/demo/ui/life_cycle_test/LifeCycleTestActivity.kt
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import com.einfoplanet.demo.R
 
 class LifeCycleTestActivity : AppCompatActivity() {
+    private lateinit var timerToast: TimerToast
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_cycle_test)
+        timerToast = TimerToast(application)
     }
 }
