58ba1bdef02fc9d5136573d0f36e050c29fd7b30
ESHVAR MALI <eshvar89@hotmail.com>
2020-08-22 23:12:23 +0530

    - added the constructor for TimerToast.kt which will take application context as parameter which is used to show the toast message

--- app/src/main/java/com/einfoplanet/demo/ui/life_cycle_test/TimerToast.kt
+++ app/src/main/java/com/einfoplanet/demo/ui/life_cycle_test/TimerToast.kt
@@ -1,8 +1,9 @@
 package com.einfoplanet.demo.ui.life_cycle_test
 
+import android.app.Application
 import android.os.CountDownTimer
 
-class TimerToast {
+class TimerToast(application: Application) {
 
     private val countDownTimer = object : CountDownTimer(60000, 3000) {
         override fun onFinish() {
